e397105aaf7765ec76834e161ba2f52861948d6b
Versun <me+github7604@versun.org>
2024-02-29 05:22:07 +0000

    Add Translated_ContentAdmin and Translated_Content model

--- django_text_translator/admin.py
+++ django_text_translator/admin.py
@@ -95,6 +95,12 @@ class ClaudeTranslatorAdmin(BaseTranslatorAdmin):
 
 
 if settings.DEBUG:
+    @admin.register(Translated_Content)
+    class Translated_ContentAdmin(admin.ModelAdmin):
+        fields = ["original_content", "translated_content", "translated_language", "tokens", "characters"]
+        list_display = ["original_content", "translated_language", "translated_content", "tokens", "characters"]
+
+
     @admin.register(TestTranslator)
     class TestTranslatorAdmin(BaseTranslatorAdmin):
         fields = ["name", "translated_text", "max_characters", "interval"]
--- django_text_translator/models.py
+++ django_text_translator/models.py
@@ -3,6 +3,7 @@ import uuid
 import json
 from time import sleep
 
+import cityhash
 import deepl
 from PyDeepLX import PyDeepLX
 import anthropic
@@ -13,6 +14,38 @@ from openai import OpenAI, AzureOpenAI
 import google.generativeai as genai
 from django.utils.translation import gettext_lazy as _
 
+class Translated_Content(models.Model):
+    hash = models.BinaryField(max_length=8, unique=True, primary_key=True, editable=False)
+    original_content = models.TextField()
+
+    translated_language = models.CharField(max_length=255)
+    translated_content = models.TextField()
+
+    tokens = models.IntegerField(default=0)
+    characters = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.original_content
+
+    @classmethod
+    def is_translated(cls, text, target_language):
+        text_hash = cityhash.CityHash64(f"{text}{target_language}").to_bytes(8, byteorder='little')
+        try:
+            content = Translated_Content.objects.get(hash=text_hash)
+            # logging.info("Using cached translations:%s", text)
+            return {
+                'text': content.translated_content,
+                'tokens': content.tokens,
+                'characters': content.characters
+            }
+        except Translated_Content.DoesNotExist:
+            logging.info("Does not exist in cache:%s", text)
+            return None
+    
+    def save(self, *args, **kwargs):
+        if not self.hash:
+            self.hash = cityhash.CityHash64(f"{self.original_content}{self.translated_language}").to_bytes(8, byteorder='little')
+        super(Translated_Content, self).save(*args, **kwargs)
 
 class TranslatorEngine(models.Model):
     name = models.CharField(_("Name"), max_length=100, unique=True)
